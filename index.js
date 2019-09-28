6348bc2961c8792fd8ab93816a6f3ff2257527cd
David Duncan <davidduncan27549@gmail.com>
2019-09-28 18:21:55 -0400

    updated api

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint);
 
 // Start server
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });

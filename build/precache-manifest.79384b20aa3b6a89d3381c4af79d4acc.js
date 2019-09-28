402fe14f8de90c8643fa8601da2c32b1524f28be
David Duncan <davidduncan27549@gmail.com>
2019-09-28 18:29:08 -0400

    full build

---
+++ build/precache-manifest.79384b20aa3b6a89d3381c4af79d4acc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "571bc686df7f4c82e4a1da1b50c5a9e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d74870542af329f6f744",
+    "url": "/static/css/main.bfdaca5c.chunk.css"
+  },
+  {
+    "revision": "bfc1d37d3075fff369de",
+    "url": "/static/js/2.fb7f2494.chunk.js"
+  },
+  {
+    "revision": "d74870542af329f6f744",
+    "url": "/static/js/main.0c35340a.chunk.js"
+  },
+  {
+    "revision": "a20c45f882cbc975ee63",
+    "url": "/static/js/runtime-main.0f94b201.js"
+  }
+]);
---
+++ build/precache-manifest.dd7cfea82efa4fe2bb367b3373e5cdc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da463e7e425ac9aa2d1da045623192c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "525c843453dce5dcddc7",
+    "url": "/static/css/main.bfdaca5c.chunk.css"
+  },
+  {
+    "revision": "bfc1d37d3075fff369de",
+    "url": "/static/js/2.fb7f2494.chunk.js"
+  },
+  {
+    "revision": "525c843453dce5dcddc7",
+    "url": "/static/js/main.f204dc70.chunk.js"
+  },
+  {
+    "revision": "a20c45f882cbc975ee63",
+    "url": "/static/js/runtime-main.0f94b201.js"
+  }
+]);
